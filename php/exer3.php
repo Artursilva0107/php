67abaffb11ca220900b6ec1ff59b437a495cd36e
Artursilva0107 <arturmwwp014@gmail.com>
2025-08-14 11:29:50 -0300

    Add files via upload

---
+++ php/exer3.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $nome = "Meu nome é garibalda ";
+    $nome2 = "meu nome é givaldo ";
+    echo $nome.$nome2
+    ?>
+</body>
+</html>
